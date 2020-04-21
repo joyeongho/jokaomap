f94755eea602cc99249efef69da14b5eafc37447
joyeongho <jo-yeong-ho@naver.com>
2020-04-21 13:44:27 +0000

    ~~~~

--- dnlcl.js
+++ dnlcl.js
@@ -4,7 +4,7 @@
 var xhr = new XMLHttpRequest();
 var url = 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid'; /*URL*/
 var queryParams = '?' + encodeURIComponent('ServiceKey') + '='+'oNJ%2FNNFWx8pMbAAWWUcxLd69nhiSbW93emPKkOH%2FQd73bUIORADpweN8i116CTEI6V3GfSJTNa3py9S%2Bs5KoEQ%3D%3D'; /*Service Key*/
-queryParams += '&' + encodeURIComponent('714') + '=' + encodeURIComponent(''); /*노선ID*/
+queryParams += '&' + encodeURIComponent('busRouteId') + '=' + encodeURIComponent('905'); /*노선ID*/
 xhr.open('GET', url + queryParams);
 xhr.onreadystatechange = function () {
     if (this.readyState == 4) {
